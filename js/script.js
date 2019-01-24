0bbebd96427e012049e7038bcd89ee1669146785
Pawel <14218944+pwodyk@users.noreply.github.com>
2019-01-24 22:20:12 +0000

    completed the solution

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
     $("tr:odd").addClass("rowOdd");
     $("tr:even").addClass("rowEven");
 
-    
+    $("th").click(function(){
+        $("td").removeClass("selection");
+        $(this).siblings("td").addClass("selection");
+    });
 
 });
